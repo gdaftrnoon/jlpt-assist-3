83d3045bee0a11f14b76478bb9310fc53baa14a1
Noon <gdaftrnoon@gmail.com>
2025-09-29 21:44:11 +0100

    reverse cache control

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async headers() {
-        return [
-            {
-                source: '/:path*',
-                has: [
-                    {
-                        type: 'cookie',
-                        key: 'shouldCache',
-                        value: 'true',
-                    },
-                ],
-                headers: [
-                    {
-                        key: 'Cache-Control',
-                        value: 'max-age=60',
-                    },
-                ],
-            },
-        ];
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
